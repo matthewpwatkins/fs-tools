44fb8453e55994e80e3aef51b7abe7b1f9f28052
Matthew Watkins <3808901+matthewpwatkins@users.noreply.github.com>
2025-01-24 09:17:49 -0600

    Better CI and use more package data in banner

--- build-scripts/post-build.js
+++ build-scripts/post-build.js
@@ -2,18 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const packageJson = require('../package.json');
 
-fs.rmSync(path.resolve(__dirname, '../dist/fs-army-knife.user.js.LICENSE.txt'), {
-  force: true,
-});
-
-const outputScriptPath = path.resolve(__dirname, '../dist/fs-army-knife.user.js');
+const outputScriptPath = path.resolve(__dirname, `../${packageJson.main}`);
 const bannerPath = path.resolve(__dirname, '../src/banner.js');
 
 if (!fs.existsSync(bannerPath)) {
   throw new Error(`Banner file not found: ${bannerPath}`);
 }
 
+const repoUrl = packageJson.repository.url.replace(/\.git$/, '');
+const installUrl = `${repoUrl}/releases/latest/download/${path.basename(packageJson.main)}`;
+
 const bannerContent = fs.readFileSync(bannerPath, 'utf8')
-  .replace('{{version}}', packageJson.version);
+  .replace('{{name}}', packageJson.monkeyScript.displayName)
+  .replace('{{version}}', packageJson.version)
+  .replace('{{description}}', packageJson.description)
+  .replace(/{{install_url}}/g, installUrl)
+  .replace('{{author}}', packageJson.author);
+
 const scriptContent = fs.readFileSync(outputScriptPath, 'utf8');
 fs.writeFileSync(outputScriptPath, bannerContent + '\n\n' + scriptContent);
--- src/banner.js
+++ src/banner.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         FamilySearch Army Knife
+// @name         {{name}}
 // @namespace    http://tampermonkey.net/
 // @version      {{version}}
-// @description  A collection of helpful utilities for working with FamilySearch
-// @author       Matthew Watkins
+// @description  {{description}}
+// @downloadURL  {{install_url}}
+// @updateURL    {{install_url}}
+// @author       {{author}}
+// @icon         https://edge.fscdn.org/assets/docs/fs_logo_favicon_sq.png
 // @match        https://www.familysearch.org/*
 // @match        https://www.findagrave.com/*
-// @icon         https://edge.fscdn.org/assets/docs/fs_logo_favicon_sq.png
-// @downloadURL  https://github.com/matthewpwatkins/fs-army-knife/releases/latest/download/fs-army-knife.user.js
-// @updateURL    https://github.com/matthewpwatkins/fs-army-knife/releases/latest/download/fs-army-knife.user.js
 // @run-at       document-start
 // @grant        GM_registerMenuCommand
 // @grant        GM_setClipboard
